42c129b19f9a59f791673aa5e1c19698b69a092c
Hiyowa Kyobashi <khiyowa@gmail.com>
2014-06-08 15:23:43 +0900

    変数と定数を追加。コメントを追記。

--- hello.rb
+++ hello.rb
@@ -5,3 +5,19 @@ p "hello world(p)"          # データの形式が分かるように表示
 #begin
 コメント
 #end
+
+# 変数: データにつけるラベル
+# 定数: 変更されないデータにつけるラベル
+
+msg = "hello world"
+p msg
+
+#begin
+変数名は好きなものをつけてもよいが、普通は英小文字から始める。
+#end
+
+ADMIN_EMAIL = "user.domain.example"
+
+#begin
+最初の文字が大文字なら定数となるが、通例、全て大文字で書く。
+#end
